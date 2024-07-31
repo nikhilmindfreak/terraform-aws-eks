41609ee31e8440d11f58d39fd398bc3869f53f70
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-07-31 19:31:39 -0400

    Initial commit

---
+++ 50-acm/variables.tf
@@ -0,0 +1,24 @@
+variable "project_name" {
+  default = "expense"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "common_tags" {
+  default = {
+    Project = "expense"
+    Environment = "dev"
+    Terraform = "true"
+    Component = "ingress-alb"
+  }
+}
+
+variable "zone_name" {
+  default = "devopsme.online"
+}
+
+variable "zone_id" {
+  default = "ZZ05782673IECJD1E1MAG1"
+}
